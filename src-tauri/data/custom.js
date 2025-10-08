dfd3ea59244f3e1662724a809dd242057939f06a
Yndex <sjj648133500@163.com>
2025-10-08 17:33:34 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,5 +1,11 @@
-console.log('custom.js ----')
-
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded')
+    document.querySelector('body').innerHTML = 'Hello World'
+    document.querySelector('body').style.backgroundColor = 'red'
+    document.querySelector('body').style.color = 'white'
+    document.querySelector('body').style.fontSize = '20px'
+    document.querySelector('body').style.fontWeight = 'bold'
+    document.querySelector('body').style.textAlign = 'center'
+    document.querySelector('body').style.padding = '20px'
+    document.querySelector('body').style.margin = '20px'
 })
